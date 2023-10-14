fd44707f666ae1170c53ac707a00e65e13b75830
saidureg <saidureg@gmail.com>
2023-10-14 17:39:08 +0600

    setup the project

---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,9 @@
+const AddCoffee = () => {
+  return (
+    <div>
+      <h3>AddCoffee</h3>
+    </div>
+  );
+};
+
+export default AddCoffee;
---
+++ src/components/UpdateCoffee.jsx
@@ -0,0 +1,9 @@
+const UpdateCoffee = () => {
+  return (
+    <div>
+      <h3>Update coffee</h3>
+    </div>
+  );
+};
+
+export default UpdateCoffee;
